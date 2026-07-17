ab3f36f4a533b0b37a79fcbb74c374b4374e9c98
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-07-17 19:02:49 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.w00lam"
+version = "0.0.1-SNAPSHOT"
+description = "Coffee order service"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/io/github/w00lam/coffeeorderservice/CoffeeOrderServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.w00lam.coffeeorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CoffeeOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
